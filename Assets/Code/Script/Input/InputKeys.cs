8c03ef36bf6db2177c6fe57945d5cbe2e1ab1321
NakaVostro <ra00297288@pucsp.edu.br>
2023-05-03 23:45:39 -0300

    Start Prototype
    Input dealing

---
+++ Assets/Code/Script/Input/InputKeys.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[SerializeField]
+public class InputKeys : MonoBehaviour {
+
+    public KeyCode LeftMovement;
+    public KeyCode RightMovement;
+    public KeyCode Jump;
+    public KeyCode Heal;
+    public KeyCode Dodge;
+
+}
