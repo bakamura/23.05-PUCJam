c9cdc3d358bacc7dfe41b5bf509c890ca899631e
NakaVostro <ra00297288@pucsp.edu.br>
2023-05-11 00:33:33 -0300

    Reorganization

---
+++ Assets/_Code/Script/Entity/Hostile/ObsessiveBehaviour.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsessiveBehaviour : HostileBehaviour {
+
+    [Header("Movement")]
+
+    [SerializeField] private float _movementSpeed;
+
+    [Header("Attack")]
+    
+    [SerializeField] private float _attackRange;
+}
